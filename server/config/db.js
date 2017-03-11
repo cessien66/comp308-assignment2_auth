e2aae4fe977d40551ba04812bb4686477d518402
cessien66 <Clintonessien@gmail.com>
2017-03-10 23:13:43 -0500

    Imaegs

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/videogames"
+  
   //Remote MongoDB deployment -> let URI = "mongodb://clinton:Mansamusa66@ds153667.mlab.com:53667/contactlist";
 };
